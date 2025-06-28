5d4a317b38a8113da7b91011d5d37732105c89b5
SmartShackMaster <fototakas@gmail.com>
2025-06-28 16:05:25 +0300

    Update Controller.h

--- components/fujitsu-halcyon/Controller.h
+++ components/fujitsu-halcyon/Controller.h
@@ -71,7 +71,7 @@ class Controller {
     };
 
     public:
-        Controller(uart_num, uint8_t controller_address, const Callbacks& callbacks, QueueHandle_t uart_event_queue = nullptr)
+        Controller(uart_port_t uart_num, uint8_t controller_address, const Callbacks& callbacks, QueueHandle_t uart_event_queue = nullptr)
             : uart_num(uart_num), controller_address(controller_address), uart_event_queue(uart_event_queue), callbacks(callbacks) {}
 
         bool start();
